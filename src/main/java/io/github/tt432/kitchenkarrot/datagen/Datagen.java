b8a84d8df6c1fcfdb17523a831639ebfa6c8914a
skyinr <2587135207@qq.com>
2024-07-30 18:02:25 +0800

    chore(DataGen): 将GLM迁移到dg

--- src/main/java/io/github/tt432/kitchenkarrot/datagen/Datagen.java
+++ src/main/java/io/github/tt432/kitchenkarrot/datagen/Datagen.java
@@ -1,17 +1,21 @@
 package io.github.tt432.kitchenkarrot.datagen;
 
 import io.github.tt432.kitchenkarrot.Kitchenkarrot;
+import io.github.tt432.kitchenkarrot.datagen.provider.MODGLMProvider;
+import io.github.tt432.kitchenkarrot.datagen.provider.ModItemModelProvider;
+import io.github.tt432.kitchenkarrot.datagen.provider.ModRecipeProvider;
+
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
 
-@Mod.EventBusSubscriber(modid = Kitchenkarrot.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = Kitchenkarrot.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class Datagen {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent e) {
@@ -20,7 +24,8 @@ public class Datagen {
         ExistingFileHelper helper = e.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = e.getLookupProvider();
 
-        generator.addProvider(true, new ModRecipeProvider(output));
+        generator.addProvider(true, new MODGLMProvider(output, lookupProvider));
+        generator.addProvider(true, new ModRecipeProvider(output, lookupProvider));
         generator.addProvider(true, new ModItemModelProvider(output, helper));
     }
 }
--- src/main/java/io/github/tt432/kitchenkarrot/datagen/provider/ModShapelessRecipeBuilder.java
+++ src/main/java/io/github/tt432/kitchenkarrot/datagen/provider/ModShapelessRecipeBuilder.java
@@ -1,4 +1,4 @@
-package io.github.tt432.kitchenkarrot.datagen;
+package io.github.tt432.kitchenkarrot.datagen.provider;
 
 import net.minecraft.advancements.CriterionTriggerInstance;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
