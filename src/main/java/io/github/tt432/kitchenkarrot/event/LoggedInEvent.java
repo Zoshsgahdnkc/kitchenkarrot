974f57f9c886bb20e7ad2553923df96a2e373dc7
Zoshsgahdnkc <2987901713@qq.com>
2025-04-21 23:55:55 +0800

    fix a wrong side bug

--- src/main/java/io/github/tt432/kitchenkarrot/event/LoggedInEvent.java
+++ src/main/java/io/github/tt432/kitchenkarrot/event/LoggedInEvent.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,7 +25,7 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.List;
 import java.util.Optional;
 
-@Mod.EventBusSubscriber(modid = Kitchenkarrot.MOD_ID)
+@Mod.EventBusSubscriber(modid = Kitchenkarrot.MOD_ID, value = Dist.CLIENT)
 public class LoggedInEvent {
     @SubscribeEvent
     public static void sendMessages(PlayerEvent.PlayerLoggedInEvent event){
